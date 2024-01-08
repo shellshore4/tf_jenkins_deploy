3f1d189676bd34877d1fd38517b973792fcfedec
shellshore4 <135078683+shellshore4@users.noreply.github.com>
2024-01-07 19:17:13 -0500

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,26 +1,26 @@
-resource "aws_security_group" "REFERENCE NAME" {
-  name        = "NAME"
+resource "aws_security_group" "allow_ssh" {
+  name        = "Jenkins SSH"
   description = "Allow inbound SSH from all IPs"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #Narrow down for security
+    cidr_blocks = ["0.0.0.0/0"] #narrow for safety
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #Narrow down #Allows port 8080 to connect to Jenkins
+    cidr_blocks = ["0.0.0.0/0"] #narrow for safety
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  vpc_id = module.vpc.vpc_id
+  vpc_id = "CHECK JENKINS_DEPLOY OUTPUT"
 }
